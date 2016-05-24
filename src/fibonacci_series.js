f5b8f7966cd3fd136c03af501df9b5a12890cb99
Ruyu You <727809166@qq.com>
2016-05-24 20:07:39 +0800

    finish fibonacci_series.js

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -1,7 +1,16 @@
 'use strict';
 
 function fibonacci_series(n) {
+    var fibonacciNumber = buildFibonacciNumber(n);
+    return fibonacciNumber;
+}
 
+function buildFibonacciNumber(n) {
+    var fibonacciNumber = [0, 1];
+    for (var i = 2; i < n + 1; i++) {
+        fibonacciNumber.push(fibonacciNumber[i - 1] + fibonacciNumber[i - 2]);
+    }
+    return fibonacciNumber;
 }
 
 module.exports = fibonacci_series;
